4d51aeded30ccdff428f4800fdadfb37e83dd6ec
Kris <ssspers92@gmail.com>
2024-02-05 16:57:00 +0300

    Upgrade get_vacancy_from_api with name in requests

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -1,6 +1,16 @@
+import requests
+
 from src.AbstractHh import AbstractHh
 
 
 class Vacancy(AbstractHh):
+
     def __init__(self, name_vacancy):
         self.name_vacancy = name_vacancy
+
+    def get_vacancy_from_api(self):
+        info = requests.get(f'https://api.hh.ru/vacancies?text=NAME:{self.name_vacancy}')
+        return info.text
+
+vacancy = Vacancy('python')
+print(vacancy.get_vacancy_from_api())
