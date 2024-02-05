30beec2af91b2eef84fa57efa214f1e74a5ae526
Kris <ssspers92@gmail.com>
2024-02-05 17:34:07 +0300

    Created test_get_vacancy_from_api()

--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -8,5 +8,10 @@ def test_issubclass():
     assert issubclass(AbstractHh, ABC)
     assert issubclass(Vacancy, AbstractHh)
 
-# def test_get_vacancy_from_api():
-#     pass
+
+def test_get_vacancy_from_api():
+    vacancy = Vacancy('python')
+    assert isinstance(vacancy.get_vacancy_from_api(), dict)
+    assert len(vacancy.get_vacancy_from_api()) != 0
+
+#     assert vacancy
