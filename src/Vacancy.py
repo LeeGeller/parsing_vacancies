2909cd69027887243b9137cef9886772d28c6640
Kris <ssspers92@gmail.com>
2024-02-07 15:50:31 +0300

    Created docstring

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -9,12 +9,12 @@ from src.AbstractHh import AbstractHh
 class Vacancy(AbstractHh):
     all = []
 
-    def __init__(self, name_vacancy):
-        self.name_vacancy = name_vacancy
+    def __init__(self, name_vacancy: str):
+        self.name_vacancy: str = name_vacancy
         self.message = "Vacancies found"
         self.save_info()
 
-    def get_vacancy_from_api(self):
+    def get_vacancy_from_api(self) -> list:
         """Get valid info about vacancies for user"""
         self.all.clear()
 
@@ -24,9 +24,9 @@ class Vacancy(AbstractHh):
 
         return self.all
 
-    def save_info(self):
+    def save_info(self) -> str or list:
         """Created json file with info about vacancies"""
-        info = self.get_vacancy_from_api()
+        info: list = self.get_vacancy_from_api()
 
         if len(self.all) == 0:
             self.message = "Vacancy not found"
