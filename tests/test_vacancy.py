39296e93aeffb65095d65ba15a708fe736bdbec6
Kris <ssspers92@gmail.com>
2024-02-07 16:48:47 +0300

    Rename Vacancy to GetVacancies

--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -1,15 +1,15 @@
 from abc import ABC
 
-from src.Vacancy import Vacancy
+from src.GetVacancies import GetVacancies
 from src.AbstractHh import AbstractHh
 
 
 def test_issubclass():
     assert issubclass(AbstractHh, ABC)
-    assert issubclass(Vacancy, AbstractHh)
+    assert issubclass(GetVacancies, AbstractHh)
 
 
 def test_save_info_and_get_vacancy_from_api():
 
-    assert Vacancy('sfsf').message == "Vacancy not found"
-    assert Vacancy('python').message == "Vacancies found"
+    assert GetVacancies('sfsf').message == "Vacancy not found"
+    assert GetVacancies('python').message == "Vacancies found"
