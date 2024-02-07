3914472e45afc7481ad14cb41c41504642e9f478
Kris <ssspers92@gmail.com>
2024-02-07 13:16:55 +0300

    Add self.message

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -9,41 +9,29 @@ from src.AbstractHh import AbstractHh
 class Vacancy(AbstractHh):
     all = []
 
-    def __init__(self, name_vacancy, salary_min):
+    def __init__(self, name_vacancy):
         self.name_vacancy = name_vacancy
-        self.salary_min = int(salary_min)
+        self.message = "Vacancies found"
         self.save_info()
 
     def get_vacancy_from_api(self):
         """Get valid info about vacancies for user"""
         self.all.clear()
-        keys_response = {'text': f'NAME:{self.name_vacancy}', 'area': 113, 'per_page': 100,
-                         "salary": self.salary_min}
+
+        keys_response = {'text': f'NAME:{self.name_vacancy}', 'area': 113, 'per_page': 100, }
         info = requests.get(f'https://api.hh.ru/vacancies', keys_response)
         self.all.extend(json.loads(info.text)['items'])
+
         return self.all
 
     def save_info(self):
         """Created json file with info about vacancies"""
-        self.sorted_all()
+        info = self.get_vacancy_from_api()
 
-        with open(DATA, 'w', encoding='utf-8') as file:
-            file.write(json.dumps(self.all, ensure_ascii=False))
-
-    def sorted_all(self):
-        """Sorted list to salary"""
-        self.get_vacancy_from_api()
-        new_list = []
-
-        for item in self.all:
-            try:
-                if item.get("salary")["from"] == self.salary_min:
-                    new_list.append(item)
-            self.all = new_list
-        return self.all
-
-
-
-vacancy = Vacancy('python', 50000)
-
-print(vacancy.all)
+        if len(self.all) == 0:
+            self.message = "Vacancy not found"
+            return self.message
+        else:
+            with open(DATA, 'w', encoding='utf-8') as file:
+                file.write(json.dumps(info, ensure_ascii=False))
+            return self.all
