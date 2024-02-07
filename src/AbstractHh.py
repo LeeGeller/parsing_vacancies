dc13021dcc530155c70c12cc1d6706d91f81eaea
Kris <ssspers92@gmail.com>
2024-02-07 17:52:49 +0300

    Formatted

--- src/AbstractHh.py
+++ src/AbstractHh.py
@@ -10,11 +10,3 @@ class AbstractHh(ABC):
     @abstractmethod
     def save_info(self):
         pass
-
-    # @abstractmethod
-    # def get_vacancy_info(self):
-    #     pass
-
-    # @abstractmethod
-    # def remove_vacancy(self):
-    #     pass
