c619682e3b0bec91e1132be020d15d906464c870
Kris <ssspers92@gmail.com>
2024-02-08 13:00:45 +0300

    Created test_get_vacancies_len_is_zero() and test_get_top_vacancies()

--- tests/test_compare_vacancies.py
+++ tests/test_compare_vacancies.py
@@ -1,4 +1,5 @@
 from src.CompareVacancies import CompareVacancies
+from copy import deepcopy
 
 
 def test_generate_salary_dict_valid():
@@ -29,3 +30,22 @@ def test_get_vacancies_check_salary():
                 check_list.append(val)
 
     assert len(check_list) == 0
+
+
+def test_get_vacancies_len_is_zero():
+    vacancy_1 = CompareVacancies('tttttttt', 300000)
+    vacancy_1.get_vacancies()
+    assert len(vacancy_1.salary_all) < 2
+    assert vacancy_1.message == "Vacancy not found"
+
+
+def test_get_top_vacancies():
+    vacancy_1 = CompareVacancies('python', 50000)
+
+    assert len(vacancy_1.get_top_vacancies()) > 0
+    top_list = []
+    for keys in vacancy_1.salary_top.keys():
+        top_list.append(keys)
+    new_list = deepcopy(top_list)
+
+    assert sorted(top_list, reverse=True) == new_list
