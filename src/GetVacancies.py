c4f1502def0503267a3bdd72e4e45582a40b4c10
Kris <ssspers92@gmail.com>
2024-02-08 13:14:36 +0300

    Upgrade functions. Add params and construction try

--- src/GetVacancies.py
+++ src/GetVacancies.py
@@ -12,14 +12,13 @@ class GetVacancies(AbstractHh):
     def __init__(self, name_vacancy: str):
         self.name_vacancy: str = name_vacancy
         self.message = "Vacancies found"
-        self.save_info()
 
-    def get_vacancy_from_api(self) -> list:
+    def get_vacancy_from_api(self, vacancy_name: str) -> list:
         """Get valid info about vacancies for user"""
         self.all.clear()
 
-        if isinstance(self.name_vacancy, str):
-            keys_response = {'text': f'NAME:{self.name_vacancy}', 'area': 113, 'per_page': 100, }
+        if isinstance(vacancy_name, str):
+            keys_response = {'text': f'NAME:{vacancy_name}', 'area': 113, 'per_page': 100, }
             info = requests.get(f'https://api.hh.ru/vacancies', keys_response)
             self.all.extend(json.loads(info.text)['items'])
             return self.all
@@ -27,14 +26,16 @@ class GetVacancies(AbstractHh):
             self.message = "Vacancy not found"
             return self.message
 
-    def save_info(self) -> str or list:
+    def save_info(self, list_info: list) -> str or list:
         """Created json file with info about vacancies"""
-        info: list = self.get_vacancy_from_api()
 
-        if len(self.all) == 0:
+        if len(list_info) == 0:
             self.message = "Vacancy not found"
             return self.message
         else:
-            with open(DATA, 'w', encoding='utf-8') as file:
-                file.write(json.dumps(info, ensure_ascii=False))
-            return self.all
+            try:
+                with open(DATA, 'w', encoding='utf-8') as file:
+                    file.write(json.dumps(list_info, ensure_ascii=False))
+                return self.all
+            except Exception:
+                raise "I can't save file."
