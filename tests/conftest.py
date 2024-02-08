dd469364fef48f82e4073e30553ed1a76fcf51ae
Kris <ssspers92@gmail.com>
2024-02-08 14:35:07 +0300

    Created fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,13 @@ from src.CompareVacancies import CompareVacancies
 @pytest.fixture
 def fixture_class_valid():
     return CompareVacancies('python')
+
+
+@pytest.fixture
+def fixture_class_number():
+    return CompareVacancies(1)
+
+
+@pytest.fixture
+def fixture_class_some_str():
+    return CompareVacancies('666999')
