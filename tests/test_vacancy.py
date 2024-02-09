ef75cca49e1dd4faaf6bc632148bf92b55189df2
Kris <ssspers92@gmail.com>
2024-02-09 12:49:59 +0300

    Formatted code

--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -17,9 +17,9 @@ def test_get_vacancy_from_api():
     vacancy2 = GetVacancies(1)
     vacancy3 = GetVacancies('python')
 
-    vacancy1.get_vacancy_from_api(vacancy1.name_vacancy)
-    vacancy2.get_vacancy_from_api(vacancy2.name_vacancy)
-    vacancy3.get_vacancy_from_api(vacancy3.name_vacancy)
+    vacancy1.get_vacancy_from_api()
+    vacancy2.get_vacancy_from_api()
+    vacancy3.get_vacancy_from_api()
 
     assert vacancy1.message == "Vacancies found"
     assert vacancy2.message == "Vacancy not found"
@@ -28,8 +28,6 @@ def test_get_vacancy_from_api():
 
 def test_save_info_valid():
     vacancy1 = GetVacancies('python')
-    list_info1 = vacancy1.get_vacancy_from_api(vacancy1.name_vacancy)
-    vacancy1.save_info(list_info1, DATA)
 
     assert isinstance(vacancy1.all, list)
     assert len(vacancy1.all) > 0
@@ -37,17 +35,6 @@ def test_save_info_valid():
 
 def test_save_info_zero_len():
     vacancy1 = GetVacancies(1)
-    list_info1 = vacancy1.get_vacancy_from_api(vacancy1.name_vacancy)
 
-    vacancy1.save_info(list_info1, DATA)
+    vacancy1.save_info()
     assert vacancy1.message == "Vacancy not found"
-
-
-def test_save_info_with_broke_path():
-    vacancy2 = GetVacancies('python')
-    list_info2 = vacancy2.get_vacancy_from_api(vacancy2.name_vacancy)
-    with pytest.raises(AttributeError):
-        vacancy2.save_info(list_info2, "src/data")
-
-    with pytest.raises(AttributeError):
-        vacancy2.save_info(list_info2, "")
