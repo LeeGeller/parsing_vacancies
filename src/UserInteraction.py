95ad66ccc490ca62dc0a7d0dc74aba187d963e53
Kris <ssspers92@gmail.com>
2024-02-10 00:13:17 +0300

    Change make_info() and created last_info()

--- src/UserInteraction.py
+++ src/UserInteraction.py
@@ -7,7 +7,7 @@ class UserInteraction(CompareVacancies):
     def __init__(self, name_vacancy):
         super().__init__(name_vacancy)
         self.get_vacancy_from_api()
-        self.vacancies = defaultdict(list)
+        self.vacancies_list = defaultdict(list)
 
     def __str__(self):
         self.message = "Vacancy not found" if len(self.all_vacancy) == 0 else self.message
@@ -18,15 +18,31 @@ class UserInteraction(CompareVacancies):
     def make_info(self, top_salary: dict) -> list:
         """
         Created list with vacancies for user.
-        :param right_top_vacancies: dict with vacancies
-        :param count_vacancies: how many vacancies
+        :param top_salary: dict with vacancies
         user wants to see
         :return: list with vacancies.
         """
         print(f"Top salary:")
+        count = 1
 
         for top, vacancies in top_salary.items():
-            print(f"Top sallary {top} - count {len(vacancies)}")
+            print(f"{count}. Top sallary {top} - count {len(vacancies)}")
+            for value in vacancies:
+                self.vacancies_list[count].extend([{"Name of vacancy": value['name']},
+                                                   {"Salary from": value['salary']['from']},
+                                                   {"Salary to": value['salary']['to']},
+                                                   {"URL": value['alternate_url']}])
+                count += 1
+
+    def last_info(self, top_salary: dict, number_of_vacancies: int):
+        """
+            Get info about top vacancies
+            :param top_salary: dict with top vacancies
+        """
+        for params_vacancy in top_salary[number_of_vacancies]:
+            for key, val in params_vacancy.items():
+                print("{0}: {1}".format(key, val))
+
 
 
 user = UserInteraction('python')
@@ -35,3 +51,5 @@ user.sorted_salary(user.all_vacancy, 50000, 'Москва')
 user.get_top_vacancies(user.sort_salary)
 
 user.make_info(user.top_salary)
+
+user.last_info(user.vacancies_list, 8)
