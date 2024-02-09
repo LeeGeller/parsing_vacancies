4df014b4717fc953024c3afd186bdc4d5564304c
Kris <ssspers92@gmail.com>
2024-02-09 23:36:09 +0300

    Created self.all_vacancy instead self.all

--- src/GetVacancies.py
+++ src/GetVacancies.py
@@ -13,14 +13,13 @@ class GetVacancies(AbstractHh):
     def __init__(self, name_vacancy: str):
         self.name_vacancy: str = name_vacancy
         self.message = "Vacancies found"
-        self.all_vacancy = self.save_info()
+        self.all_vacancy = self.get_vacancy_from_api()
 
     def __repr__(self):
         return f"{self.all_vacancy}"
 
     def get_vacancy_from_api(self) -> list:
         """Get valid info about vacancies for user"""
-        self.all.clear()
 
         if isinstance(self.name_vacancy, str):
             keys_response = {'text': f'NAME:{self.name_vacancy}', 'area': 113, 'per_page': 100, }
@@ -32,12 +31,11 @@ class GetVacancies(AbstractHh):
 
     def save_info(self) -> str or list:
         """Created json file with info about vacancies"""
-        self.get_vacancy_from_api()
 
-        if len(self.all) == 0:
+        if len(self.all_vacancy) == 0:
             self.message = "Vacancy not found"
             return self.message
         else:
             with open(DATA, 'w', encoding='utf-8') as file:
-                file.write(json.dumps(self.all, ensure_ascii=False))
-            return self.all
+                file.write(json.dumps(self.all_vacancy, ensure_ascii=False))
+            return self.all_vacancy
