2c38afb75a966b8b201aeaedf1db929483490159
Kris <ssspers92@gmail.com>
2024-02-10 14:04:18 +0300

    Created classmethod

--- src/JsonSaver.py
+++ src/JsonSaver.py
@@ -5,12 +5,14 @@ from src.AbstractJsonSaver import AbstractJsonSaver
 
 
 class JsonSaver(AbstractJsonSaver):
-    def save_file(self, data: list):
+    @classmethod
+    def save_file(cls, data: list):
         """Save file"""
         with open(DATA, 'w', encoding='utf-8') as file:
             file.write(json.dumps(data, indent=2, ensure_ascii=False))
 
-    def read_file(self):
+    @classmethod
+    def read_file(cls):
         """Read file"""
         with open(DATA, encoding='utf-8') as file:
             return json.load(file)
