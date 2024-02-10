9e02f33cbb8e10f8d389a4c412dbf1f1868d607c
Kris <ssspers92@gmail.com>
2024-02-10 11:36:30 +0300

    rename GetVacancy

--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -3,19 +3,19 @@ from abc import ABC
 import pytest
 
 from config import DATA
-from src.ApiHH import GetVacancies
+from src.ApiHH import ApiHH
 from src.AbstractHh import AbstractHh
 
 
 def test_issubclass():
     assert issubclass(AbstractHh, ABC)
-    assert issubclass(GetVacancies, AbstractHh)
+    assert issubclass(ApiHH, AbstractHh)
 
 
 def test_get_vacancy_from_api():
-    vacancy1 = GetVacancies('tttttt')
-    vacancy2 = GetVacancies(1)
-    vacancy3 = GetVacancies('python')
+    vacancy1 = ApiHH('tttttt')
+    vacancy2 = ApiHH(1)
+    vacancy3 = ApiHH('python')
 
     vacancy1.get_vacancy_from_api()
     vacancy2.get_vacancy_from_api()
@@ -27,13 +27,13 @@ def test_get_vacancy_from_api():
 
 
 def test_save_info_valid():
-    vacancy1 = GetVacancies('python')
+    vacancy1 = ApiHH('python')
 
     assert isinstance(vacancy1.all_vacancy, list)
     assert len(vacancy1.all_vacancy) > 0
 
 
 def test_save_info_zero_len():
-    vacancy1 = GetVacancies(1)
+    vacancy1 = ApiHH(1)
 
     assert vacancy1.save_info() == "Vacancy not found"
