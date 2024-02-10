1c498782079fe7cf6d86f3392d95d299d4ca87f4
Kris <ssspers92@gmail.com>
2024-02-10 14:13:17 +0300

    Created classmethods for work with file

--- src/JsonSaver.py
+++ src/JsonSaver.py
@@ -2,6 +2,7 @@ import json
 
 from config import DATA
 from src.AbstractJsonSaver import AbstractJsonSaver
+from src.GetApiHh import GetApiHh
 
 
 class JsonSaver(AbstractJsonSaver):
@@ -16,3 +17,21 @@ class JsonSaver(AbstractJsonSaver):
         """Read file"""
         with open(DATA, encoding='utf-8') as file:
             return json.load(file)
+
+    @classmethod
+    def add_vacancy_to_file(cls, data: list):
+        old_list = cls.read_file()
+        new_list = data + old_list
+        cls.save_file(new_list)
+
+    @classmethod
+    def delete_vacancy(cls, vacancy: str):
+        new_list = []
+
+        old_list = cls.read_file()
+
+        for params in old_list:
+            if params['name'] != vacancy:
+                new_list.append(params)
+
+        cls.save_file(new_list)
