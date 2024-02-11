b472961cc6ce9b2ce9100ee5c964f6b8098f767d
Kris <ssspers92@gmail.com>
2024-02-11 13:27:56 +0300

    Add main

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
-from src.UserInteraction import UserInteraction
+from src.GetApiHh import GetApiHh
+from src.JsonSaver import JsonSaver
+from src.Vacancy import Vacancy
 
+response = GetApiHh()
+response.get_vacancy_from_api('python')
+file_json = JsonSaver()
+file_json.save_file(response.all_vacancy)
+file_vacancies = file_json.read_file()
+vacancy = Vacancy.get_vacancy_list(file_vacancies, 'Москва', 50000)
 
-def main():
-    user_input = input("Please, write name\nof vacancy for search: ")
-
-    while True:
-        users_salary = input("Text salary if you want\n"
-                             "to see vacancies with salary:\n")
-        if users_salary.isdigit():
-            break
-        print("\nPlease, text number or 'Enter...\n")
-
-    user = UserInteraction(user_input)
-    user.get_vacancy_from_api()
-
-    while True:
-        users_city = input("Now, you need text your city:\n").capitalize()
-        if users_city.isalpha():
-            break
-        print("\nI don't know about this city.\n")
-
-    user.sorted_salary(user.all_vacancy, int(users_salary), users_city)
-    user.get_top_vacancies(user.sort_salary)
-
-    user.make_info(user.top_salary)
-
-    while True:
-        number_vacancy = input("Choose number of top vacancy\n"
-                               "if you want to see more: ")
-        if number_vacancy.isdigit():
-            break
-
-    print(user.last_info(user.vacancies_list, number_vacancy))
-
-
-if __name__ == '__main__':
-    main()
+print(*sorted(vacancy))
