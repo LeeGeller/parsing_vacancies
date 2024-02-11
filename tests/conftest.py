b0dd3be089bc92bbb0fcc53b4ea8185ed2fd3899
Kris <ssspers92@gmail.com>
2024-02-11 15:04:12 +0300

    Add fixture_class_get_hh_valid,fixture_class_get_hh_negative

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,36 @@
-import json
-
+# import json
+#
+# import pytest
+#
+# from config import DATA_TEST
+# from src.Vacancy import CompareVacancies
+#
+#
 import pytest
 
-from config import DATA_TEST
-from src.Vacancy import CompareVacancies
+from src.GetApiHh import GetApiHh
 
 
 @pytest.fixture
-def fixture_class_valid():
-    return CompareVacancies('python')
+def fixture_class_get_hh_valid():
+    return GetApiHh().get_vacancy_from_api('python')
 
 
 @pytest.fixture
-def fixture_class_number():
-    return CompareVacancies(1)
-
-
-@pytest.fixture
-def fixture_class_some_str():
-    return CompareVacancies('666999')
-
-
-@pytest.fixture
-def fixture_data():
-    with open(DATA_TEST, encoding='utf-8') as file:
-        return json.loads(file.read())
-
+def fixture_class_get_hh_negative():
+    return GetApiHh().get_vacancy_from_api("1")
+# @pytest.fixture
+# def fixture_class_number():
+#     return CompareVacancies(1)
+#
+#
+# @pytest.fixture
+# def fixture_class_some_str():
+#     return CompareVacancies('666999')
+#
+#
+# @pytest.fixture
+# def fixture_data():
+#     with open(DATA_TEST, encoding='utf-8') as file:
+#         return json.loads(file.read())
+#
