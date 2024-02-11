0ff9bb951b4ef0b91087fac599460c8db565cf48
Kris <ssspers92@gmail.com>
2024-02-11 15:20:44 +0300

    Formatted code

--- src/JsonSaver.py
+++ src/JsonSaver.py
@@ -2,36 +2,32 @@ import json
 
 from config import DATA
 from src.AbstractJsonSaver import AbstractJsonSaver
-from src.GetApiHh import GetApiHh
 
 
 class JsonSaver(AbstractJsonSaver):
-    @classmethod
-    def save_file(cls, data: list):
+
+    def save_file(self, data: list):
         """Save file"""
         with open(DATA, 'w', encoding='utf-8') as file:
             file.write(json.dumps(data, indent=2, ensure_ascii=False))
 
-    @classmethod
-    def read_file(cls):
+    def read_file(self):
         """Read file"""
         with open(DATA, encoding='utf-8') as file:
             return json.load(file)
 
-    @classmethod
-    def add_vacancy_to_file(cls, data: list):
-        old_list = cls.read_file()
+    def add_vacancy_to_file(self, data: list):
+        old_list = self.read_file()
         new_list = data + old_list
-        cls.save_file(new_list)
+        self.save_file(new_list)
 
-    @classmethod
-    def delete_vacancy(cls, vacancy: str):
+    def delete_vacancy(self, vacancy: str):
         new_list = []
 
-        old_list = cls.read_file()
+        old_list = self.read_file()
 
         for params in old_list:
             if params['name'] != vacancy:
                 new_list.append(params)
 
-        cls.save_file(new_list)
+        self.save_file(new_list)
