33633979b862f8291bcf39525b3673c2126faed4
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2024-03-06 20:29:57 +0300

    Reformatted code

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
-from src.parsing_hh import GetApiHh, JsonSaver, Vacancy
+from config import config
+from src.parsing_hh import GetApiHh, JsonSaver, Vacancy, DBManager
 
-response = GetApiHh()
 
-while True:
-    user_vacancy: str = input("Text name vacancy for search:\n")
-    user_city: str = input("Text city in where you want to find vacancies:\n")
-    if isinstance(user_city, str) and isinstance(user_vacancy, str):
-        break
-    print(f"Please, text alpha, not {user_city}")
+def main():
+    response = GetApiHh()
 
-while True:
-    user_salary = input("Text min salary for search:\n")
-    if user_salary.isdigit():
-        break
-    print(f"Please, text digit, not {user_salary}")
+    while True:
+        user_vacancy: str = input("Text name vacancy for search:\n")
+        user_city: str = input("Text city in where you want to find vacancies:\n")
+        if isinstance(user_city, str) and isinstance(user_vacancy, str):
+            break
+        print(f"Please, text alpha, not {user_city}")
 
-# Get vacancies for user
-response.get_vacancy_from_api(user_vacancy)
+    while True:
+        user_salary = input("Text min salary for search:\n")
+        if user_salary.isdigit():
+            break
+        print(f"Please, text digit, not {user_salary}")
 
-file_json = JsonSaver()
+    # Get vacancies for user
+    response.get_vacancy_from_api(user_vacancy)
 
-# Save response to JSON
-file_json.save_file(response.all_vacancy)
+    file_json = JsonSaver()
 
-# Read JSON file
-file_vacancies = file_json.read_file()
+    # Save response to JSON
+    file_json.save_file(response.all_vacancy)
 
-# Print vacancies for user
-vacancy = Vacancy.get_vacancy_list(file_vacancies, user_city, int(user_salary))
-sorted_vacancies = sorted(vacancy)
+    # Read JSON file
+    file_vacancies = file_json.read_file()
 
-while True:
-    count = input("How vacancies do you want to see:\n")
-    if count.isdigit():
-        break
-    print(f"Please, text digit, not {count}")
+    # Sorted file with vacancies
+    sorted_list = Vacancy.sorted_vacancy_list(file_vacancies, user_city, int(user_salary))
 
-print(*sorted_vacancies[:int(count)])
+    # Print vacancies for user
+    vacancy = Vacancy.get_vacancy_list(sorted_list)
+    sorted_vacancies = sorted(vacancy)
+
+    while True:
+        count = input("How vacancies do you want to see:\n")
+        if count.isdigit():
+            break
+        print(f"Please, text digit, not {count}")
+
+    print(*sorted_vacancies[:int(count)])
+
+    d_base = DBManager()
+    params = config()
+
+    # Create database
+    d_base.create_data_base('vacancies', params)
+
+    # Create tables
+    d_base.create_table('vacancies', params)
+
+    # Save info to database
+    d_base.save_data_to_database(sorted_vacancies, 'vacancies', params)
+
+
+if __name__ == '__main__':
+    main()
