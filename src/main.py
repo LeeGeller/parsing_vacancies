bb1a47c037c3891657a3fc9bd156d80bf7a6f581
Kris <ssspers92@gmail.com>
2024-08-22 21:50:32 +0300

    main

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from database.queries.core import create_table, insert_data
+from src.parsing_vacancies import ApiHh, ApiHabr
+
+query_vacancies_list = ["python", "junior", "Python"]
+
+hh = ApiHh()
+habr = ApiHabr()
+
+dirty_list_hh = hh.get_vacancy_from_api(query_vacancies_list, pages_limit=2)
+clean_list_hh = hh.clean_vacancies_list(dirty_list_hh)
+
+dirty_list_habr = habr.get_vacancy_from_api(query_vacancies_list, pages_limit=2)
+clean_list_habr = habr.clean_vacancies_list(dirty_list_habr)
+
+create_table()
+insert_data(clean_list_hh + clean_list_habr)
