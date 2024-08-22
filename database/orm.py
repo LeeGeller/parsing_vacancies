d0d7655dcda7e57451729e5823e79f5c649e24e8
Kris <ssspers92@gmail.com>
2024-08-22 22:14:44 +0300

    Upgrade code

--- database/orm.py
+++ database/orm.py
@@ -7,10 +7,10 @@ class VacanciesORM(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name_vacancy: Mapped[str]
-    company: Mapped[str] = mapped_column(nullable=True)
-    location: Mapped[str] = mapped_column(nullable=True)
-    description: Mapped[str] = mapped_column(nullable=True)
-    url: Mapped[str] = mapped_column(nullable=True)
-    experience: Mapped[str] = mapped_column(nullable=True)
-    salary_from: Mapped[int] = mapped_column(nullable=True)
-    salary_to: Mapped[int] = mapped_column(nullable=True)
+    company: Mapped[str | None]
+    location: Mapped[str | None]
+    description: Mapped[str | None]
+    url: Mapped[str | None]
+    experience: Mapped[str | None]
+    salary_from: Mapped[int]
+    salary_to: Mapped[int]
